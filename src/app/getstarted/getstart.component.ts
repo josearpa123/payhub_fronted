48a3b2c1220d0d13848f7e95388ed5f2b830ab82
Jose David Arias <joseariaspantoj@gmail.com>
2025-03-19 14:12:52 -0500

    Se realizo la parte de obtener cuenta

--- src/app/getstarted/getstart.component.ts
+++ src/app/getstarted/getstart.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-getstart',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './getstart.component.html',
   styleUrl: './getstart.component.css'
 })
